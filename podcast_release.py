c4d0d8e1738879716addb7134b32b94e4aff8348
Alexander Troitsky <troitsky@tochka.com>
2023-07-22 11:14:54 +0300

    name_changed

--- podcast_release.py
+++ podcast_release.py
@@ -77,7 +77,7 @@ def create_podcast_text(prompt):
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=messages,
-        max_tokens=10000,
+        max_tokens=7000,
         n=1,
         temperature=0,
     )
