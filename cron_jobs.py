88cc9322ce8a2577b4f73b1dd183ee4ccacda4a2
Alexander Troitsky <troitsky@tochka.com>
2023-11-29 19:23:52 +0000

    requirements are cleaner

--- cron_jobs.py
+++ cron_jobs.py
@@ -24,12 +24,12 @@ def run_script_telegpt():
 
 
 # Schedule the task
-daily_time_telega = "10:41"
-daily_time_podcast = "19:31"
-daily_time_telegpt = "11:11"
+daily_time_telega = "07:41"
+daily_time_podcast = "18:39"
+daily_time_telegpt = "14:11"
 schedule.every().day.at(daily_time_telega).do(run_script_telega)
 schedule.every().day.at(daily_time_podcast).do(run_script_podcast)
-schedule.every().day.at(daily_time_podcast).do(run_script_telegpt)
+schedule.every().day.at(daily_time_telegpt).do(run_script_telegpt)
 
 # Keep the script running indefinitely to check for scheduled tasks
 while True:
