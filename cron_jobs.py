9c5a2ebefe34978b2edaf5db05822b1131cef582
troimc <troitsky.a.a@yandex.ru>
2024-02-20 20:24:45 +0000

    cron_jobs updated

--- cron_jobs.py
+++ cron_jobs.py
@@ -12,7 +12,7 @@ def run_script_telega():
 
 
 def run_script_podcast():
-    script_path = 'podcast_release.py'
+    script_path = 'podcast_release_short.py'
     os.system(f'C:\\Users\\Administrator\\PycharmProjects\\AI_content_creator\\venv\\Scripts\\python {script_path}')
     print(f'Script executed at {datetime.now()}')
 
@@ -23,13 +23,23 @@ def run_script_telegpt():
     print(f'Script executed at {datetime.now()}')
 
 
+def run_script_analytics():
+    script_path = 'tg_telegpt_analytics.py.py'
+    os.system(f'C:\\Users\\Administrator\\PycharmProjects\\AI_content_creator\\venv\\Scripts\\python {script_path}')
+    print(f'Script executed at {datetime.now()}')
+
+
 # Schedule the task
 daily_time_telega = "08:41"
 daily_time_podcast = "18:39"
 daily_time_telegpt = "08:11"
+daily_time_analytics = "20:30"
+
 schedule.every().day.at(daily_time_telega).do(run_script_telega)
 schedule.every().day.at(daily_time_podcast).do(run_script_podcast)
 schedule.every().day.at(daily_time_telegpt).do(run_script_telegpt)
+schedule.every().day.at(daily_time_analytics).do(run_script_analytics)
+
 
 # Keep the script running indefinitely to check for scheduled tasks
 while True:
